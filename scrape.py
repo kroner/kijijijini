245fc07c76c92c930f3663b48d673973ef64ef76
Robert Krone <rckrone@gmail.com>
2020-10-26 15:08:31 -0400

    python packages

--- scrape.py
+++ scrape.py
@@ -9,7 +9,7 @@ import categories
 from datetime import datetime
 from bs4 import BeautifulSoup
 from concurrent.futures import as_completed
-from requests_futures.sessions import FuturesSession
+#from requests_futures.sessions import FuturesSession
 
 url = 'https://www.kijiji.ca/b-{0}/{1}/page-{2}/c{3}l{4}'
 SLEEP_TIME = 60 # seconds to wait if no results are returned
