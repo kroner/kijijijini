02e9737cab833cd4b81264147353a92620f2984d
Robert Krone <rckrone@gmail.com>
2020-12-03 23:10:52 -0500

    forgot commands.py

--- commands.py
+++ commands.py
@@ -11,7 +11,7 @@ import sys
 def create(table):
     if table == 'items':
         Item.__table__.create(db.engine)
-        #for item in categories.item_dict:
+        #for item in categories.all_items():
         #    database.add_item(item)
     if table == 'listings':
         Listing.__table__.create(db.engine)
@@ -26,7 +26,7 @@ def drop(table):
 
 # Add listings from csv files to the database
 def read_csvs():
-    for item in categories.item_dict:
+    for item in categories.items():
         Item.get(item)
     #for item in ['art-collectables']:
         try:
@@ -37,15 +37,29 @@ def read_csvs():
         print(item, file=sys.stdout)
 
 # Scrape new listings from kijiji and add them to the database
-@click.argument("x")
-def update(x):
-    if x == 'database':
-        Listing.update()
-    if x == 'model':
-        model.train_all()
+@click.argument("cat")
+def update(cat):
+    if cat == 'all':
+        items = categories.items()
+    else:
+        items = categories.by_name(cat).active_children()
+    for item in items:
+        Listing.update(item)
 
+# Retrain the models
+@click.argument("cat")
+def train(cat):
+    if cat == 'all':
+        cats = categories.categories()
+    else:
+        cats = [categories.by_name(cat)]
+    for cat in cats:
+        model.train(cat)
+
+def fix():
+    Item.query.filter_by(Item.id==12).delete()
 
 def init_app(app):
-    commands = [create, drop, read_csvs, update]
+    commands = [create, drop, read_csvs, update, train, fix]
     for command in commands:
         app.cli.add_command(app.cli.command()(command))
