0384bbef568ccb865313f139b426752a159f4f7c
Robert Krone <rckrone@gmail.com>
2020-12-08 21:16:30 -0500

    .

--- chart.py
+++ chart.py
@@ -9,6 +9,7 @@ import model
 
 def prepare_chart_data(df, item, aggregate=True):
     df = df.dropna()
+    df = df[datetime.date.fromisoformat('2020-08-10') < df['post_date']]
     df['date'] = df['post_date'].apply(lambda x : x.isoformat())
     df['cat_id'] = df['item_id'].apply(lambda x : categories.by_id(x).category().id)
     df_items = df[['item_id', 'date', 'count', 'logsum']].groupby(['item_id', 'date']).sum().reset_index()
